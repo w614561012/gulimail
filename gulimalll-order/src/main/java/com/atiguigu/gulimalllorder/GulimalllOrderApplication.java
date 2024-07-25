7fc30a4d2687951dd0fa6d4b9151ef26bab183e4
w614561012 <614561012@qq.com>
2024-07-25 23:01:05 +0800

    demo

---
+++ gulimalll-order/src/main/java/com/atiguigu/gulimalllorder/GulimalllOrderApplication.java
@@ -0,0 +1,13 @@
+package com.atiguigu.gulimalllorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimalllOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimalllOrderApplication.class, args);
+    }
+
+}
---
+++ gulimalll-order/src/test/java/com/atiguigu/gulimalllorder/GulimalllOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atiguigu.gulimalllorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimalllOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
